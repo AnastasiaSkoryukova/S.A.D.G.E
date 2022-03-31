3cac1492bbb97a39b3dd25815c0797a63ba87aae
Simon Italia <simonitaliaatwork@gmail.com>
2022-03-31 17:34:51 +0200

    modify model with scheme name and db fields

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -2,27 +2,38 @@ import Fluent
 import Vapor
 
 final class Todo: Model, Content {
-    static let schema = "todos"
+    static let schema = "public"
     
     @ID(key: .id)
-    var id: UUID?
+    var id: Int
 
     @Field(key: "city")
     var city: String
     
+    @Field(key: "lat")
+    var latitude: String
+    
+    @Field(key: "lng")
+    var longitude: String
+    
     @Field(key: "country")
     var country: String
     
+    @Field(key: "iso2")
+    var iso2: String
+    
+    @Field(key: "admin_name")
+    var adminName: String
+    
+    @Field(key: "capital")
+    var capital: String
+    
     @Field(key: "population")
     var population: String
-
+    
     init() { }
 
-    init(id: UUID? = nil, title: String) {
-        self.id = id
-        self.city = city
-        self.country = country
-        self.population = population
-        
+    init(title: String) {
+        self.title = title
     }
 }
